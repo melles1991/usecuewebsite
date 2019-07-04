36d10c716f4936ec4b4aff23594ca574e2ae545a
Joost van der Schee <jhvanderschee@gmail.com>
2019-07-05 01:38:39 +0200

    update

--- _site/js/javascript.js
+++ _site/js/javascript.js
@@ -119,6 +119,8 @@ document.addEventListener("DOMContentLoaded", documentReady);
                         event.preventDefault();
                         document.getElementById('lightbox').innerHTML = '<a id="close"></a><a id="next">&rsaquo;</a><a id="prev">&lsaquo;</a><div class="videoWrapperContainer"><div class="videoWrapper"><iframe src="https://player.vimeo.com/video/'+el.getAttribute('data-id')+'/?autoplay=1&byline=0&title=0&portrait=0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe></div></div>';
                         document.getElementById('lightbox').style.display = 'block';
+
+                        setGallery(this);
                     });
                 }
                 else if (xmlhttp.status == 400) {
@@ -219,8 +221,9 @@ document.addEventListener("DOMContentLoaded", documentReady);
             element.addEventListener("click", function(event) {
                 event.preventDefault();
                 document.getElementById('lightbox').innerHTML = '<a id="close"></a><a id="next">&rsaquo;</a><a id="prev">&lsaquo;</a><div class="img" style="background: url(\''+this.getAttribute('href')+'\') center center / contain no-repeat;" title="'+this.getAttribute('title')+'" ><img src="'+this.getAttribute('href')+'" alt="'+this.getAttribute('title')+'" /></div><span>'+this.getAttribute('title')+'</span>';
-                
                 document.getElementById('lightbox').style.display = 'block';
+
+                setGallery(this);
             });
         });
 
