8435fa42aa702f4eaec34486397339ac24071966
Joost van der Schee <joost@vdschee.nl>
2019-10-02 10:50:25 +0200

    update

--- js/javascript.js
+++ js/javascript.js
@@ -6,3 +6,11 @@ layout:
 {% include js/newwindowfix.js %}
 {% include js/smoothscroll.js %}
 {% include js/lightbox.js %}
+
+function toggleDarkmode() {
+    document.getElementById('top').classList.toggle('darkmode'); 
+    document.getElementById('darkmode').classList.toggle('selected'); 
+    if (localStorage.getItem('mode') != 'dark') localStorage.setItem('mode', 'dark'); 
+    else localStorage.setItem('mode', 'normal');
+    document.getElementById('darkmode').blur();
+}
