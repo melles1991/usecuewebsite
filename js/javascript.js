f21ff1b7f59651626e42ab76f41f376c3810fa18
Joost van der Schee <joost@vdschee.nl>
2020-02-24 00:26:51 +0100

    update

--- js/javascript.js
+++ js/javascript.js
@@ -10,7 +10,7 @@ function toggleLightmode() {
     if(document.getElementById('top').classList.contains('lightmode')){
         document.getElementById('top').classList.remove('lightmode');
         document.getElementById('top').classList.add('darkmode');
-        localStorage.setItem('mode', 'light'); 
+        localStorage.setItem('mode', 'dark'); 
     } else if (document.getElementById('top').classList.contains('darkmode')) {
         document.getElementById('top').classList.remove('darkmode');
         localStorage.setItem('mode', 'blue'); 
