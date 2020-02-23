e2ad484810dbb02b4abb465bf1b43f35e69fb274
Joost van der Schee <joost@vdschee.nl>
2020-02-24 00:15:31 +0100

    update

--- js/javascript.js
+++ js/javascript.js
@@ -6,10 +6,17 @@ layout:
 {% include js/newwindowfix.js %}
 {% include js/lightbox.js %}
 
-function toggleDarkmode() {
-    document.getElementById('top').classList.toggle('darkmode'); 
-    document.getElementById('darkmode').classList.toggle('selected'); 
-    if (localStorage.getItem('mode') != 'dark') localStorage.setItem('mode', 'dark'); 
-    else localStorage.setItem('mode', 'normal');
-    document.getElementById('darkmode').blur();
+function toggleLightmode() {
+    if(document.getElementById('top').classList.contains('lightmode')){
+        document.getElementById('top').classList.remove('lightmode');
+        document.getElementById('top').classList.add('darkmode');
+        localStorage.setItem('mode', 'light'); 
+    } else if (document.getElementById('top').classList.contains('darkmode')) {
+        document.getElementById('top').classList.remove('darkmode');
+        localStorage.setItem('mode', 'blue'); 
+    } else {
+        document.getElementById('top').classList.add('lightmode');
+        localStorage.setItem('mode', 'light'); 
+    }
+    document.getElementById('lightmode').blur();
 }
