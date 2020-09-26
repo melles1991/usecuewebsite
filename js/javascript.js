f62d1720819d3a04bc446c085b4f70a3225829c0
Joost van der Schee <joost@vdschee.nl>
2020-09-26 21:34:06 +0200

    update

--- js/javascript.js
+++ js/javascript.js
@@ -40,3 +40,7 @@ if(document.body.classList.contains('error') && window.location.href.indexOf("/n
         elements[i].style.display = 'none';
     }
 }
+
+document.body.addEventListener('mousemove', function() {
+    document.body.classList.add('loaded');
+});
